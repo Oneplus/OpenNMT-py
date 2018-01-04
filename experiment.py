d23fc26ec62ea11bbf870b82f7f54bc5e0bd189c
Oneplus <oneplus.lau@gmail.com>
2018-01-04 13:50:45 +0800

    fix

--- experiment.py
+++ experiment.py
@@ -7,10 +7,13 @@ def main():
     cmd = argparse.ArgumentParser('experiment.py')
     cmd.add_argument('-n', required=True, type=int, help='the number of models')
     cmd.add_argument('-explore_type', required=True, help='the exploration type [teacher_forcing, boltzmann]')
+    cmd.add_argument('-anneal', default=1., type=float, help='the annealing parameter')
     cmd.add_argument('-alpha', default=1., help='the alpha, only used when explore_type=teacher forcing.')
-    cmd.add_argument('-epsilon', default=0, help='the epsilon in the ')
+    cmd.add_argument('-epsilon', default=0, help='the epsilon in the e-greedy')
+    cmd.add_argument('-temperature', defautl=1, type=float, help='the temperature in boltzmann')
     cmd.add_argument('-topk', required=True, type=int, help='the number of k-best')
     cmd.add_argument('-renormalize', default=False, action='store_true', help='do re-normalize.')
+    cmd.add_argument('-seed', default=1, help='the seed')
     opts = cmd.parse_args()
     explore_type = opts.explore_type
     assert explore_type in ('teacher_forcing', 'boltzmann', 'epsilon_greedy')
@@ -18,8 +21,10 @@ def main():
         explore_type += '_{0}'.format(opts.alpha)
     elif explore_type == 'epsilon_greedy':
         explore_type += '_{0}'.format(opts.epsilon)
+    elif explore_type == 'boltzmann':
+        explore_type += '_{0}'.format(opts.temperature)
 
-    params = 'x{0}.{1}.top{2}.{3}'.format(opts.n, explore_type, opts.topk, 'norm' if opts.renormalize else 'nonorm')
+    params = 'x{0}.{1}.an{2}.top{3}.s{4}.{5}'.format(opts.n, explore_type, opts.anneal, opts.topk, opts.seed, 'norm' if opts.renormalize else 'nonorm')
     directory = os.path.join('saves', params)
     if not os.path.isdir(directory):
         os.makedirs(directory)
@@ -37,12 +42,15 @@ def main():
                 '-explore_type', '{explore}'.format(explore=opts.explore_type),
                 '-report_every', '1',
                 '-topk', '{k}'.format(k=opts.topk),
+                '-annealing', str(opts.anneal),
                 '-gpu', '0']
         # Generate scripts
         if opts.explore_type == 'teacher_forcing':
             cmds.extend(['-distill_alpha', '{alpha}'.format(alpha=opts.alpha)])
         elif opts.explore_type == 'epsilon_greedy':
             cmds.extend(['-epsilon_greedy_epsilon', '{epsilon}'.format(epsilon=opts.epsilon)])
+        elif opts.explore_type == 'boltzmann':
+            cmds.extend(['-boltzmann_temperature', '{temperature}'.format(epsilon=opts.temperature)])
 
         if opts.renormalize:
             cmds.append('-renormalize')
@@ -54,6 +62,8 @@ def main():
             if i > 0 and cmds[i - 1] == '-explore_type':
                 cmds[i] = 'teacher_forcing'
             if i > 0 and cmds[i - 1] == '-distill_alpha':
+                cmds[i] = '1'
+            if i > 0 and cmds[i - 1] == '-annealing':
                 cmds[i] = '1'
         extra_cmds = ['-data', 'iwslt2014/data.valid',
                       '-save_data', '{dir}/data.valid'.format(dir=directory)]
@@ -68,10 +78,17 @@ def main():
                     '-distill',
                     '-data', '{dir}/data'.format(dir=directory),
                     '-save_model', '{dir}/distill-model-{optim}'.format(dir=directory, optim=optim),
+                    '-src_word_vec_size', '256',
+                    '-tgt_word_vec_size', '256',
+                    '-layers', '1',
+                    '-enc_layers', '1',
+                    '-dec_layers', '1',
+                    '-dropout', '0.2',
                     '-rnn_size', '256',
                     '-gpuid', '0',
-                    '-seed', '1',
+                    '-seed', opts.seed,
                     '-epochs', '30',
+                    '-start_decay_at', '20',
                     '-valid_txt', 'data/src-val.txt',
                     '-valid_output', '{dir}/val-runtime-output.txt'.format(dir=directory),
                     '-valid_script', './tools/iwslt_val_bleu.sh']
